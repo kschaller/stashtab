4e8d22871e996721e0f5f40d04d49fe0c90b9c53
Kai Schaller <kschaller@gmail.com>
2018-06-12 07:49:57 -0700

    Plan out general script execution

--- stash.js
+++ stash.js
@@ -1,1 +1,39 @@
-// Note: use chrome.storage.sync so that saved tab groups save in the cloud
+chrome.browserAction.onClicked.addListener(function() {
+
+});
+
+/*
+[
+  {
+    name: "Group 1",
+    urls: [
+      "http://google.com",
+      "http://www.nytimes.com"
+    ]
+  },
+]
+*/
+
+/*
+
+1. Retrieve a list of stored objects
+
+2. Create HTML list elements with a loop. Assign index as a value for later use.
+
+3. Save a new one? Just reload the whole table for simplicity.
+
+*/
+
+var tabGroups = [];
+
+var testGroup = {
+  name: "Test Group",
+  urls: [
+    "http://google.com",
+    "http://www.nytimes.com"
+  ]
+}
+
+// chrome.storage.sync.get('tabGroups', function(data) {
+
+// });
